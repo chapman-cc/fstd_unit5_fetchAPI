73fa1a986d21ca3cf118bc601e7acaa9a8ea79ef
chapmanCC <chapboy1989@gmail.com>
2019-05-16 16:49:32 +0800

    add error catch

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const randUserMulti = "?results=12"
 const gallery = new Gallery();
 
 fetch(randUserAPI + randUserMulti)
-.then(res => res.json())
-.then(data => gallery.addPeople(data))
-.then(() => gallery.appendToGallery())
+    .then(res => res.json())
+    .then(data => gallery.addPeople(data))
+    .then(() => gallery.appendToGallery())
+    .catch(err => console.log(Error(err)))
