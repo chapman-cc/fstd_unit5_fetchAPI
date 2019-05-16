ae8552b8c99c9051e48f7e2e0c04023d5597a790
chapmanCC <chapboy1989@gmail.com>
2019-05-16 17:33:08 +0800

    rename gallery.gallery to gallery.galleryDOM

--- js/galleryClass.js
+++ js/galleryClass.js
@@ -1,6 +1,6 @@
 class Gallery {
     constructor() {
-        this.gallery = document.querySelector("#gallery"); 
+        this.galleryDOM = document.querySelector("#gallery"); 
         this.people = [];
     }
     
@@ -10,7 +10,9 @@ class Gallery {
     appendToGallery() {
         this.people.forEach(person => {
             const div = person.generateCardDiv();
-            this.gallery.appendChild(div)
+            this.galleryDOM.appendChild(div)
         })
     }
+
+
 }
