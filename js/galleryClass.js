99f9e82fee97e511228fb73101de3fd88682ad00
chapmanCC <chapboy1989@gmail.com>
2019-05-17 20:21:36 +0800

    add gallery.insertModalContainer()

--- js/galleryClass.js
+++ js/galleryClass.js
@@ -4,6 +4,13 @@ class Gallery {
         this.people = [];
     }
     
+    insertModalContainer() {
+        const div = document.createElement("div");
+        div.id = "modal-container";
+        this.modalDom = div; // ? is it good to set a property inside a method?
+        this.galleryDom.insertAdjacentElement("afterend", div);
+    }
+    
     addPeople(json) {
         json.results.forEach(person => this.people.push(new Person(person)))
     }
